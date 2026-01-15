bb75f2aa485f8423c8c9ad071846eff1d3dd4c12
shashankk-42 <shashankkakad10@gmail.com>
2026-01-15 17:53:36 +0530

    Fix Express route pattern for compatibility with path-to-regexp

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
     });
 }
